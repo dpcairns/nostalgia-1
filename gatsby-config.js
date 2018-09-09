cc6b9b946975b081c440c1581bab0bd368eeaf20
dpcairns <dpcairns@gmail.com>
2018-09-08 18:41:18 -0700

    experiment with baseurl and permalinks

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
       {
         resolve: `gatsby-source-wordpress`,
         options: {
-          baseUrl: `46.101.251.228/nostalgia-1/index.php`,
+          baseUrl: `46.101.251.228/nostalgia-1`,
           protocol: `http`,
           hostingWPCOM: false,
           verboseOutput: true,
